8ef0dc2579940708d041afc221a0cb2622296c1f
Alyosha228 <ivan.gorin123456@gmail.com>
2019-01-10 18:48:05 +0700

    :):):):):)

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello world!!')
+
+import sys
+
+print('Hello world')
+
+for i in range(10):
+    print(i)
+g = sys.platform
+if "linux" in g:
+    print("оу май.................................................................")
+print('Hello Надя')
+
+# эту строчку я добавил с сайта
+# de pranque
+# klass
