7e66c8c5574845c7b2b64257fd195f6578206118
Alyosha228 <ivan.gorin123456@gmail.com>
2019-01-10 18:57:29 +0700

    :):):):):)

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-import sys
 
+import sys
 
 print('Hello world')
 
-# for i in range(10):
-#     print(i)
-
+for i in range(10):
+    print(i)
 g = sys.platform
-k = "\\"
-print(sys.prefix)
-print(f"{(sys.prefix).split(sep=k)[2]}, теперь я знаю твоё имя..................................")
 if "linux" in g:
-    print("оу май.................................................................")
-
+    print("оу май..........................................................")
 print('Hello Надя')
 
-# эту строчку я добавrtyил с сайта
+# эту строчку я добавил с сайта
 # de pranque
 # klass
